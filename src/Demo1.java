d12c549304ac75807402dde90af37367ff8b6980
pratutake26 <100688030+pratutake26@users.noreply.github.com>
2022-03-02 22:38:56 +0530

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -4,7 +4,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hii pratiksha");
 		//This is my name
-	system.out.printil();
+	System.out.println("my name is pratiksha");
 	}
 
 }
