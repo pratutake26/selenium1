4b17dffe637eb36b1c38b2f75c704b5abd77fdaf
pratutake26 <100688030+pratutake26@users.noreply.github.com>
2022-03-03 21:36:53 +0530

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -4,8 +4,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hii pratiksha");
 		//This is my name
-	System.out.println("my name is pratiksha");
-	System.out.println("Hii from local");
+		System.out.println("Hii from local");
 	}
 
 }
